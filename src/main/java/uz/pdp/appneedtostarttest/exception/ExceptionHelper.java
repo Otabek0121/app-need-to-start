9a7826b1a03a007211e9f4570fb81a1e19c4a4cc
Rajabov Otabek <130150428+Otabek0121@users.noreply.github.com>
2025-06-04 17:30:18 +0500

    Update ExceptionHelper.java

--- src/main/java/uz/pdp/appneedtostarttest/exception/ExceptionHelper.java
+++ src/main/java/uz/pdp/appneedtostarttest/exception/ExceptionHelper.java
@@ -348,13 +348,13 @@ public class ExceptionHelper {
         //AGAR RESOURSE TOPILMAGANLIGI XATOSI BO'LSA CLIENTGA QAYSI TABLEDA NIMA TOPILMAGANLIGI HAQIDA XABAR QAYTADI
         if (ex.getFieldName() != null)
             return new ResponseEntity<>(ApiResult.errorResponse(ex.getUserMsg(), ex.getErrorCode()), HttpStatus.BAD_REQUEST);
-//            return new ResponseEntity<>(ApiResult.errorResponse(ex.getUserMsg(), ex.getErrorCode()), ex.getStatus());
+
         //AKS HOLDA DOIMIY EXCEPTIONLAR ISHLAYVERADI
         if (ex.getErrors() != null)
             return new ResponseEntity<>(ApiResult.errorResponse(ex.getErrors()), HttpStatus.BAD_REQUEST);
-//            return new ResponseEntity<>(ApiResult.errorResponse(ex.getErrors()), ex.getStatus());
+
         return new ResponseEntity<>(ApiResult.errorResponse(ex.getUserMsg(), ex.getErrorCode() != null ? ex.getErrorCode() : ex.getStatus().value()), HttpStatus.BAD_REQUEST);
-//        return new ResponseEntity<>(ApiResult.errorResponse(ex.getUserMsg(), ex.getErrorCode() != null ? ex.getErrorCode() : ex.getStatus().value()), ex.getStatus());
+
     }
 
 
